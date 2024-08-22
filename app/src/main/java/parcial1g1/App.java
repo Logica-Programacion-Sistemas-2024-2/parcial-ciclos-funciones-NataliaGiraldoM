0449a112743bd31b83420619a6b8c58f9a06e6f5
NataliaGiraldoM <natalia.giraldom.col@upb.edu.co>
2024-08-22 07:28:26 -0500

    Solución Parcial

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -3,22 +3,65 @@ package parcial1g1;
 
 public class App {
 
-    public static void main(String[] args) {
-        
-        // Implementar la logica de solucion
+ public static void main(String[] args) {
+
+        try {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Favor ingresar la cantidad de vehiculos: ");
+            int cantidadVehiculos = sc.nextInt();
+            sc.nextLine();
+
+            float metrosVehiculo = 0f;
+            float kmRecorrido = 0f;
+            String consumoVehiculo = "";
+            float cantidadCombustible = 0f;
+            float totalCombustible = 0f; 
+
+
+            for (int i = 1; i <= cantidadVehiculos; i++){
+
+                System.out.println("Ingrese los metros recorridos por el vehiculo: " + i);
+                metrosVehiculo = sc.nextFloat();
+
+                if (metrosVehiculo < 0){
+                    System.out.println("Valor inválido, favor ingresar un número correcto: ");
+                }
+
+                else { 
+
+                kmRecorrido = metrosVehiculo / 1000;
+
+                cantidadCombustible = calcularConsumoGasolina(kmRecorrido);
+
+                consumoVehiculo = "\nVehículo " + i +  ": " + kmRecorrido + " km";
+
+                System.out.println(consumoVehiculo + " ---> " + cantidadCombustible + " litros de gasolina");
+
+                totalCombustible += cantidadCombustible;
+
+                }
+
+            }
+
+            System.out.println("\nLa cantidad total de combustible fue: " + totalCombustible + " litros");
+            
+        } catch (Exception e) {
+           System.out.println("Error");
+        }
         
     }
 
-    /*  Implemente una funcion llamada calcularConsumoGasolina que cumpla con las siguientes caracteristicas
-        * Recibe: 
-            - Un float representando los km recorridos
-        * Retorna:
-            - Un float representando los litros de gasolina consumidos
-    */
-    //------------------------------------------------------------------
-    //Implemente la funcion en este espacio
-    
+    public static float calcularConsumoGasolina(float kmRecorrido){
 
-    //------------------------------------------------------------------
+        try {
+            return ((kmRecorrido * 55) / 750); 
+
+        } catch (Exception e) {
+            return -1;
+        }
+      
+    }
 
 }
